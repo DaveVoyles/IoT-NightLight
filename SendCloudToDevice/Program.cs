3ea3cd879ee37626e634522811b65d02f7d3fb57
Dave Voyles <Dave Voyles>
2016-03-23 16:40:40 -0400

    request feeback from IoT device when message is sent

--- SendCloudToDevice/Program.cs
+++ SendCloudToDevice/Program.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure.Devices;
+using System.Linq;
 
 namespace SendCloudToDevice
 {
@@ -16,6 +17,7 @@ namespace SendCloudToDevice
         {
             Console.WriteLine("Send Cloud-to-Device message\n");
             serviceClient = ServiceClient.CreateFromConnectionString(connString);
+            ReceiveFeedbackAsync();
 
             Console.WriteLine("Press any key to send a C2D message.");
             Console.ReadLine();
@@ -27,7 +29,29 @@ namespace SendCloudToDevice
         private async static Task SendCloudToDeviceMessageAsync()
         {
             var commandMessage = new Message(Encoding.ASCII.GetBytes("Cloud to device message."));
+            // In order to request feedback for the delivery of your cloud-to-device message, you have to specify a property
+            commandMessage.Ack = DeliveryAcknowledgement.Full;
             await serviceClient.SendAsync(DeviceToReceiveMsg, commandMessage);
+        }
+
+
+
+        private async static void ReceiveFeedbackAsync()
+        {
+            var feedbackReceiver = serviceClient.GetFeedbackReceiver();
+
+            Console.WriteLine("\nReceiving c2d feedback from service");
+            while (true)
+            {
+                var feedbackBatch = await feedbackReceiver.ReceiveAsync();
+                if (feedbackBatch == null) continue;
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Received feedback: {0}", string.Join(", ", feedbackBatch.Records.Select(f => f.StatusCode)));
+                Console.ResetColor();
+
+                await feedbackReceiver.CompleteAsync(feedbackBatch);
+            }
         }
 
     }
