819bc76d6602433d22be8420364df350d23d62b8
Dave Voyles <Dave Voyles>
2016-03-24 06:16:22 -0400

    Changes

---
+++ ReadDeviceToCloudMessages/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+
+namespace ReadDeviceToCloudMessages
+{
+    class Program
+    {
+        static string connectionString = "HostName=dv-iot-labs.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=leERufTFaeRxPx7o9KN7w0Abc8Sl+Y6S11AkLo6iHFI=";
+        static string iotHubD2cEndpoint = "messages/events";
+        static EventHubClient eventHubClient;
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Receive messages\n");
+            eventHubClient = EventHubClient.CreateFromConnectionString(connectionString, iotHubD2cEndpoint);
+
+            var d2cPartitions = eventHubClient.GetRuntimeInformation().PartitionIds;
+
+            foreach (string partition in d2cPartitions)
+            {
+                ReceiveMessagesFromDeviceAsync(partition);
+            }
+            Console.ReadLine();
+        }
+
+
+        private async static Task ReceiveMessagesFromDeviceAsync(string partition)
+        {
+            var eventHubReceiver = eventHubClient.GetDefaultConsumerGroup().CreateReceiver(partition, DateTime.UtcNow);
+            while (true)
+            {
+                EventData eventData = await eventHubReceiver.ReceiveAsync();
+                if (eventData == null) continue;
+
+                string data = Encoding.UTF8.GetString(eventData.GetBytes());
+                Console.WriteLine(string.Format("Message received. Partition: {0} Data: '{1}'", partition, data));
+            }
+        }
+    }
+}
--- SendCloudToDevice/Program.cs
+++ SendCloudToDevice/Program.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.Azure.Devices;
 using System.Linq;
 
+
 namespace SendCloudToDevice
 {
     class Program
